a501eac95cc5f603d1ea6a6352bf59c341cb7772
dima <dima@gmail.com>
2018-02-02 19:59:52 +0200

    vk3

--- vk/api.js
+++ vk/api.js
@@ -51,7 +51,7 @@ var getImages = function () {
                 try{
                     body = JSON.parse(chunk);
                 }
-                catch { continue; }
+                catch(err) { continue; }
                 if(body && body.response && body.response[1].media && body.response[1].media.thumb_src){
                     path = body.response[1].media.thumb_src;
                     https.get(path, function (res) {
