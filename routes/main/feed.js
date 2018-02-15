b79b5b9e474089bddc2c7ce2d369177ccb84636c
dima <dima@gmail.com>
2018-02-15 16:15:59 +0200

    -getCategoriesFeed

--- routes/main/feed.js
+++ routes/main/feed.js
@@ -22,36 +22,36 @@ module.exports = function(app, passport) {
           res.json({ memes: data.rows });   
         })  
   });
-  app.get("/feed/getCategoriesFeed",  passport.authenticate('jwt', { session: false }), function(req, res){      
-    if(req.query.count && req.query.offset && req.user.accesslvl != -1){
-      var count = req.query.count;
-      var offset = req.query.offset;
-    } else return res.status(400).json({message: "incorrect query"})
-    db.query('SELECT categoryname FROM categories', [], (err, data) => {
-      var catsString = '';
-      for(var i = 0; i < data.rows.length; i++){
-        catsString += data.rows[i].categoryname;
-        catsString += ', ';
-      }
-      catsString = catsString.slice(0, -2);
-      db.query(`SELECT ${catsString} FROM users WHERE userid = ${req.user.userid}`, [], (err, data) => {
-        var ob = data.rows[0];
-        var str = '';
-        for(var prop in ob){
-          if(ob[prop] == '1'){
-            str += prop + ' = ' + ob[prop] ; 
-          }
-          str += ' AND ';
-        }
-        str = str.substring(0, str.length - 5);
-        db.query('SELECT images.imageid, likes, dislikes, likes.opinion AS opinion '
-            +`FROM images LEFT OUTER JOIN likes ON likes.imageid = images.imageid AND likes.userid = $1 WHERE ${str}` 
-            +'ORDER BY imageid DESC LIMIT $2 OFFSET $3', [req.user.userid, count, offset], (err, data) => {
-          res.json({ memes: data.rows });   
-        }) 
-      })
-    })
-  });
+  // app.get("/feed/getCategoriesFeed",  passport.authenticate('jwt', { session: false }), function(req, res){      
+  //   if(req.query.count && req.query.offset && req.user.accesslvl != -1){
+  //     var count = req.query.count;
+  //     var offset = req.query.offset;
+  //   } else return res.status(400).json({message: "incorrect query"})
+  //   db.query('SELECT categoryname FROM categories', [], (err, data) => {
+  //     var catsString = '';
+  //     for(var i = 0; i < data.rows.length; i++){
+  //       catsString += data.rows[i].categoryname;
+  //       catsString += ', ';
+  //     }
+  //     catsString = catsString.slice(0, -2);
+  //     db.query(`SELECT ${catsString} FROM users WHERE userid = ${req.user.userid}`, [], (err, data) => {
+  //       var ob = data.rows[0];
+  //       var str = '';
+  //       for(var prop in ob){
+  //         if(ob[prop] == '1'){
+  //           str += prop + ' = ' + ob[prop] ; 
+  //         }
+  //         str += ' AND ';
+  //       }
+  //       str = str.substring(0, str.length - 5);
+  //       db.query('SELECT images.imageid, likes, dislikes, likes.opinion AS opinion '
+  //           +`FROM images LEFT OUTER JOIN likes ON likes.imageid = images.imageid AND likes.userid = $1 WHERE ${str}` 
+  //           +'ORDER BY imageid DESC LIMIT $2 OFFSET $3', [req.user.userid, count, offset], (err, data) => {
+  //         res.json({ memes: data.rows });   
+  //       }) 
+  //     })
+  //   })
+  // });
   app.get("/feed/getCategoryFeed",  passport.authenticate('jwt', { session: false }), function(req, res){      
     if(req.query.count && req.query.offset && req.query.categoryname){
       var count = req.query.count;
