8f03baaeab5260a789a3bd95592ebaef04462c9d
nikita <shevchenkona19@gmail.com>
2018-02-18 15:08:10 +0300

    '3'

--- routes/main/config.js
+++ routes/main/config.js
@@ -32,62 +32,62 @@ module.exports = function (app, passport) {
                 res.status(200).json({message: "200"});
             })
 
-
-        });
-        app.post("/config/postPhoto", passport.authenticate('jwt', {session: false}), function (req, res) {
-            if (req.user.accesslvl == -1) {
-                return res.status(400).json({message: "unauthorized"});
-            }
-            if (req.body.imagedata) {
-                var imagedata = req.body.imagedata;
-            } else return res.status(400).json({message: "incorrect data"});
-            db.query('UPDATE users SET imagedata = $1 WHERE userid = $2', [imagedata, req.user.userid], (err, data) => {
-                res.status(200).json({message: "200"});
-            })
-        });
-        app.get("/config/getPersonalCategories", passport.authenticate('jwt', {session: false}), function (req, res) {
-            if (req.user.accesslvl == -1) {
-                return res.status(400).json({message: "unauthorized"});
-            }
-            db.query('SELECT categoryid, categoryname FROM categories', [], (err, data) => {
-                var catsString = '';
-                var ids = [];
-                for (var i = 0; i < data.rows.length; i++) {
-                    catsString += data.rows[i].categoryname;
-                    catsString += ', ';
-                    ids.push(data.rows[i].categoryid);
-                }
-                catsString = catsString.slice(0, -2);
-                db.query(`SELECT ${catsString} FROM users WHERE userid = ${req.user.userid}`, [], (err, data) => {
-                    var ob = data.rows[0];
-                    var arr = [];
-                    var j = 0;
-                    for (var prop in ob) {
-                        arr.push({categoryname: prop, categoryIsUsed: ob[prop], categoryId: ids[j]});
-                        j++;
-                    }
-                    res.json({categories: arr});
-                })
-            })
-        });
-    };
-
-    var setCategory = (userid, categoryid) => {
-        db.query('SELECT categoryname FROM categories WHERE categoryid = $1', [categoryid], (err, data) => {
-            var categoryname = data.rows[0].categoryname;
-            console.log(categoryname);
-            db.query(`UPDATE users SET ${categoryname} = '1' WHERE userid = ${userid}`, [], (err, data) => {
-            })
         })
-    }
-
-    var getCategoriesArray = () => {
-        db.query('SELECT categoryname FROM categories', [], (err, data) => {
-            var result = [];
+    });
+    app.post("/config/postPhoto", passport.authenticate('jwt', {session: false}), function (req, res) {
+        if (req.user.accesslvl == -1) {
+            return res.status(400).json({message: "unauthorized"});
+        }
+        if (req.body.imagedata) {
+            var imagedata = req.body.imagedata;
+        } else return res.status(400).json({message: "incorrect data"});
+        db.query('UPDATE users SET imagedata = $1 WHERE userid = $2', [imagedata, req.user.userid], (err, data) => {
+            res.status(200).json({message: "200"});
+        })
+    });
+    app.get("/config/getPersonalCategories", passport.authenticate('jwt', {session: false}), function (req, res) {
+        if (req.user.accesslvl == -1) {
+            return res.status(400).json({message: "unauthorized"});
+        }
+        db.query('SELECT categoryid, categoryname FROM categories', [], (err, data) => {
+            var catsString = '';
+            var ids = [];
             for (var i = 0; i < data.rows.length; i++) {
-                result[i] = data.rows[i].categoryname;
+                catsString += data.rows[i].categoryname;
+                catsString += ', ';
+                ids.push(data.rows[i].categoryid);
             }
-            console.log(result);
-            return result;
+            catsString = catsString.slice(0, -2);
+            db.query(`SELECT ${catsString} FROM users WHERE userid = ${req.user.userid}`, [], (err, data) => {
+                var ob = data.rows[0];
+                var arr = [];
+                var j = 0;
+                for (var prop in ob) {
+                    arr.push({categoryname: prop, categoryIsUsed: ob[prop], categoryId: ids[j]});
+                    j++;
+                }
+                res.json({categories: arr});
+            })
         })
-    }
+    });
+};
+
+var setCategory = (userid, categoryid) => {
+    db.query('SELECT categoryname FROM categories WHERE categoryid = $1', [categoryid], (err, data) => {
+        var categoryname = data.rows[0].categoryname;
+        console.log(categoryname);
+        db.query(`UPDATE users SET ${categoryname} = '1' WHERE userid = ${userid}`, [], (err, data) => {
+        })
+    })
+}
+
+var getCategoriesArray = () => {
+    db.query('SELECT categoryname FROM categories', [], (err, data) => {
+        var result = [];
+        for (var i = 0; i < data.rows.length; i++) {
+            result[i] = data.rows[i].categoryname;
+        }
+        console.log(result);
+        return result;
+    })
+}
