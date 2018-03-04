6920eccb75364591e238bfb15c919a59d5e46a07
dima <dima@gmail.com>
2018-03-04 10:23:51 +0200

    'x'

--- routes/main/favorites.js
+++ routes/main/favorites.js
@@ -17,8 +17,7 @@ module.exports = function(app, passport) {
       } else return res.status(200).json({ message:"200" });
       favarr.push(id);
       console.log(favarr);
-      db.query(`UPDATE users SET favories = '${favarr}' WHERE userid = ${req.user.userid}`, [], (err, data) => {
-        console.log(`UPDATE users SET favories = '${favarr}' WHERE userid = ${req.user.userid}`);
+      db.query(`UPDATE users SET favories = '${JSON.stringify(favarr)}' WHERE userid = ${req.user.userid}`, [], (err, data) => {
         console.log(`UPDATE users SET favories = '${JSON.stringify(favarr)}' WHERE userid = ${req.user.userid}`);
         res.status(200).json({ message: "200" });
       })
