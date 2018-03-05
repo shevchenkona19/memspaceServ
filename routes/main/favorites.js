b93d1edc13c6bb07072bf8582fc49d40a8da835b
dima <dima@gmail.com>
2018-03-05 10:59:04 +0200

    f

--- routes/main/favorites.js
+++ routes/main/favorites.js
@@ -1,37 +1,39 @@
 var db = require('../../model/db');
 
-module.exports = function(app, passport) {
-  app.get("/favorites/addToFavorites",  passport.authenticate('jwt', { session: false }), function(req, res){      
-    if(req.query.id && req.user.accesslvl != -1){
+module.exports = function (app, passport) {
+  app.get("/favorites/addToFavorites", passport.authenticate('jwt', { session: false }), function (req, res) {
+    if (req.query.id && req.user.accesslvl != -1) {
       var id = req.query.id;
     } else return res.status(400).json({ message: "incorrect data" });
     db.query('SELECT favorites FROM users WHERE userid = $1', [req.user.userid], (err, data) => {
-      if(data && data.rows[0]){
-        var favarr = JSON.parse(data.rows[0].favorites); 
-        for(var i = 0; i < favarr.length; i++){
-          if(favarr[i] == id) return res.status(200).json({ message: "already favorite" });
+      if (data && data.rows[0]) {
+        var favarr = JSON.parse(data.rows[0].favorites);
+        for (var i = 0; i < favarr.length; i++) {
+          if (favarr[i] == id) return res.status(200).json({ message: "already favorite" });
         }
         // favarr.forEach(element => {
         //   if(element == id) return res.status(200).json({ message: "already favorite" });
         // });
-      } else return res.status(200).json({ message:"200" });
+      } else return res.status(200).json({ message: "200" });
       favarr.push(id);
       console.log(favarr);
       db.query(`UPDATE users SET favorites = '${JSON.stringify(favarr)}' WHERE userid = ${req.user.userid}`, [], (err, data) => {
         console.log(`UPDATE users SET favories = '${JSON.stringify(favarr)}' WHERE userid = ${req.user.userid}`);
         res.status(200).json({ message: "200" });
       })
-    }) 
+    })
   });
-  app.get("/favorites/getAllFavorites",  passport.authenticate('jwt', { session: false }), function(req, res){
-    if(req.user.accesslvl != -1){
-    } else return res.status(400).json({ message: "unauthorized" });      
+  app.get("/favorites/getAllFavorites", passport.authenticate('jwt', { session: false }), function (req, res) {
+    if (req.user.accesslvl != -1) {
+    } else return res.status(400).json({ message: "unauthorized" });
     db.query('SELECT favorites FROM users WHERE userid = $1', [req.user.userid], (err, data) => {
-      res.json(data.rows[0]);
-    }) 
+      if (data) {
+        res.json({ favorites: JSON.parse(data.rows[0].favorites) });
+      }
+    })
   });
-  app.get("/favorites/removeFromFavorites",  passport.authenticate('jwt', { session: false }), function(req, res){      
-    if(req.query.id && req.user.accesslvl != -1){
+  app.get("/favorites/removeFromFavorites", passport.authenticate('jwt', { session: false }), function (req, res) {
+    if (req.query.id && req.user.accesslvl != -1) {
       var id = req.query.id;
     } else return res.status(400).json({ message: "incorrect data" });
     db.query('SELECT favorites FROM users WHERE userid = $1', [req.user.userid], (err, data) => {
@@ -41,7 +43,7 @@ module.exports = function(app, passport) {
       } else res.status(200).json({ message: "not a favorite" });
       db.query(`UPDATE users SET favorites = '${JSON.stringify(favarr)}' WHERE userid = ${req.user.userid}`, [], (err, data) => {
         res.status(200).json({ message: "200" });
-      }) 
-    }) 
+      })
+    })
   });
 };
