2ed7ade0141a868c2b594d847d157c078d413ffc
dima <dima@gmail.com>
2018-03-09 14:33:19 +0200

    ''

--- routes/main/account.js
+++ routes/main/account.js
@@ -1,6 +1,10 @@
 var jwt = require('jsonwebtoken');
 var db = require('../../model/db');
 var fs = require('fs');
+var bcrypt = require('bcrypt');
+
+var salt = bcrypt.genSaltSync(10);
+console.log(salt);
 
 module.exports = function(app, passport, jwtOptions) {
     app.post('/account/login', (req, res) => {      
@@ -8,13 +12,18 @@ module.exports = function(app, passport, jwtOptions) {
             var username = req.body.username;
             var password = req.body.password;
           } else return res.status(400).json({ message: "incorrect data" });
-          db.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (err, data) => {
+          db.query('SELECT userid, password FROM users WHERE username = $1', [username], (err, data) => {
+            if(err)
             if(data.rows[0]){
-              var payload = {id: data.rows[0].userid};
-              var token = jwt.sign(payload, jwtOptions.secretOrKey);
-              res.json({token: token});
+              if (bcrypt.hashSync(password, salt) === data.rows[0].password) {
+                var payload = {id: data.rows[0].userid};
+                var token = jwt.sign(payload, jwtOptions.secretOrKey);
+                return res.json({token: token});
+              }      
+              
+              
             } else {
-              res.status(401).json({message:"no such user found"});
+              return res.status(401).json({message:"no such user found"});
             }
           })
     });
@@ -29,15 +38,15 @@ module.exports = function(app, passport, jwtOptions) {
         var password = req.body.password;
         var email = req.body.email;
       } else {
-        res.status(400).json({message:"not full info"});
-        return;
+        return res.status(400).json({message:"not full info"});
       }
       db.query('SELECT COUNT(*) as cnt FROM users WHERE username = $1 OR email = $2', [username, email], (err, data) => {
         if(data && data.rows[0].cnt == 0){
             fs.readFile('noimage.png', function(err, image) {
-              if (err) throw err;  
-              db.query('INSERT INTO users(username, password, email, imagedata) VALUES($1, $2, $3, $4)', [username, password, email, image], (err, data) => {
-                db.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (err, data) => {
+              if (err) throw err; 
+              var passwordToSave = bcrypt.hashSync(password, salt);
+              db.query('INSERT INTO users(username, password, email, imagedata) VALUES($1, $2, $3, $4)', [username, passwordToSave, email, image], (err, data) => {
+                db.query('SELECT userid FROM users WHERE username = $1', [username], (err, data) => {
                   if(data.rows[0]){
                     var payload = {id: data.rows[0].userid};
                     var token = jwt.sign(payload, jwtOptions.secretOrKey);
