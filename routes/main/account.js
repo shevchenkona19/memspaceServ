3864bbbc91de4bea4f4d5dc9cff586a9ed508d04
dima <dima@gmail.com>
2018-03-09 17:11:33 +0200

    ''

--- routes/main/account.js
+++ routes/main/account.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var bcrypt = require('bcrypt');
 
 var salt = process.env.BCRYPTSALT;
+var saltRounds = process.env.SALTROUNDS;
 
 module.exports = function (app, passport, jwtOptions) {
   app.post('/account/login', (req, res) => {
@@ -17,11 +18,13 @@ module.exports = function (app, passport, jwtOptions) {
         return res.status(500).json({ message: "BD error" });
       }
       if (data.rows[0]) {
-        if (bcrypt.hashSync(password, salt) === data.rows[0].password) {
-          var payload = { id: data.rows[0].userid };
-          var token = jwt.sign(payload, jwtOptions.secretOrKey);
-          return res.json({ token: token });
-        } else return res.status(401).json({ message: "passwords do not match" });
+        bcrypt.compare(password, hash, (err, res) => {
+          if (res) {
+            var payload = { id: data.rows[0].userid };
+            var token = jwt.sign(payload, jwtOptions.secretOrKey);
+            return res.json({ token: token });
+          } else return res.status(401).json({ message: "passwords do not match" });
+        });
       } else {
         return res.status(401).json({ message: "no such user found" });
       }
@@ -44,26 +47,27 @@ module.exports = function (app, passport, jwtOptions) {
             console.log(err.stack);
             return res.status(500).json({ message: "default image error" });
           }
-          var passwordToSave = bcrypt.hashSync(password, salt);
-          db.query('INSERT INTO users(username, password, email, imagedata) VALUES($1, $2, $3, $4)', [username, passwordToSave, email, image], (err, data) => {
-            if (err) {
-              console.log(err.stack);
-              return res.status(500).json({ message: "BD error" });
-            }
-            db.query('SELECT userid FROM users WHERE username = $1', [username], (err, data) => {
+          bcrypt.hash(password, saltRounds, function(err, hash) {
+            db.query('INSERT INTO users(username, password, email, imagedata) VALUES($1, $2, $3, $4)', [username, hash, email, image], (err, data) => {
               if (err) {
                 console.log(err.stack);
                 return res.status(500).json({ message: "BD error" });
               }
-              if (data.rows[0] && data.rows[0].userid) {
-                var payload = { id: data.rows[0].userid };
-                var token = jwt.sign(payload, jwtOptions.secretOrKey);
-                return res.json({ token: token });
-              } else {
-                return res.status(500).json({ message: "BD error" });
-              }
+              db.query('SELECT userid FROM users WHERE username = $1', [username], (err, data) => {
+                if (err) {
+                  console.log(err.stack);
+                  return res.status(500).json({ message: "BD error" });
+                }
+                if (data.rows[0] && data.rows[0].userid) {
+                  var payload = { id: data.rows[0].userid };
+                  var token = jwt.sign(payload, jwtOptions.secretOrKey);
+                  return res.json({ token: token });
+                } else {
+                  return res.status(500).json({ message: "BD error" });
+                }
+              })
             })
-          })
+          });
         });
       } else {
         return res.status(400).json({ message: "username or email is already taken" });
