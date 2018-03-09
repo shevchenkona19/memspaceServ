1e041b26905f0862f8baa5861b5255cd931cf0a0
dima <dima@gmail.com>
2018-03-09 14:55:48 +0200

    ''

--- routes/main/account.js
+++ routes/main/account.js
@@ -3,62 +3,70 @@ var db = require('../../model/db');
 var fs = require('fs');
 var bcrypt = require('bcrypt');
 
-var salt = bcrypt.genSaltSync(10);
-console.log(salt);
+var salt = process.env.BCRYPTSALT;
 
-module.exports = function(app, passport, jwtOptions) {
-    app.post('/account/login', (req, res) => {      
-        if(req.body.username && req.body.password){
-            var username = req.body.username;
-            var password = req.body.password;
-          } else return res.status(400).json({ message: "incorrect data" });
-          db.query('SELECT userid, password FROM users WHERE username = $1', [username], (err, data) => {
-            if(data.rows[0]){
-              if (bcrypt.hashSync(password, salt) === data.rows[0].password) {
-                var payload = {id: data.rows[0].userid};
-                var token = jwt.sign(payload, jwtOptions.secretOrKey);
-                return res.json({token: token});
-              }      
-              
-              
-            } else {
-              return res.status(401).json({message:"no such user found"});
-            }
-          })
-    });
-    app.get("/account/getMyUsername", passport.authenticate('jwt', { session: false }), function(req, res){ 
-      if(req.user.accesslvl != -1){
-      } else return res.status(400).json({ message: "unauthorized" });
-      res.json({"username":req.user.username});     
-    });
-    app.post('/account/register', (req, res) => {
-      if(req.body.username && req.body.password && req.body.email){
-        var username = req.body.username;
-        var password = req.body.password;
-        var email = req.body.email;
-      } else {
-        return res.status(400).json({message:"not full info"});
+module.exports = function (app, passport, jwtOptions) {
+  app.post('/account/login', (req, res) => {
+    if (req.body.username && req.body.password) {
+      var username = req.body.username;
+      var password = req.body.password;
+    } else return res.status(400).json({ message: "incorrect data" });
+    db.query('SELECT userid, password FROM users WHERE username = $1', [username], (err, data) => {
+      if (err) {
+        console.log(err.stack);
+        return res.status(500).json({ message: "BD error" });
       }
-      db.query('SELECT COUNT(*) as cnt FROM users WHERE username = $1 OR email = $2', [username, email], (err, data) => {
-        if(data && data.rows[0].cnt == 0){
-            fs.readFile('noimage.png', function(err, image) {
-              if (err) throw err;
-              console.log(password);
-              var passwordToSave = bcrypt.hashSync(password, salt);
-              console.log(passwordToSave);
-              db.query('INSERT INTO users(username, password, email, imagedata) VALUES($1, $2, $3, $4)', [username, passwordToSave, email, image], (err, data) => {
-                db.query('SELECT userid FROM users WHERE username = $1', [username], (err, data) => {
-                  if(data.rows[0]){
-                    var payload = {id: data.rows[0].userid};
-                    var token = jwt.sign(payload, jwtOptions.secretOrKey);
-                    res.json({token: token});
-                  }
-                }) 
-              })  
-            }); 
-        } else {
-          res.status(400).json({message:"already registered"});
-        }
-      })
-    });
+      if(data) console.log('data true');
+      if(data.rows) console.log('data.rows true');
+      if(data.rows[0]) console.log('data.rows[0] true');
+      if(data.rows[0].password) console.log('data.rows[0].password true');
+      return res.json({ });
+      // if (data.rows[0].password) {
+      //   if (bcrypt.hashSync(password, salt) === data.rows[0].password) {
+      //     var payload = { id: data.rows[0].userid };
+      //     var token = jwt.sign(payload, jwtOptions.secretOrKey);
+      //     return res.json({ token: token });
+      //   }
+
+
+      // } else {
+      //   return res.status(401).json({ message: "no such user found" });
+      // }
+    })
+  });
+  app.get("/account/getMyUsername", passport.authenticate('jwt', { session: false }), function (req, res) {
+    if (req.user.accesslvl != -1) {
+    } else return res.status(400).json({ message: "unauthorized" });
+    res.json({ "username": req.user.username });
+  });
+  app.post('/account/register', (req, res) => {
+    if (req.body.username && req.body.password && req.body.email) {
+      var username = req.body.username;
+      var password = req.body.password;
+      var email = req.body.email;
+    } else {
+      return res.status(400).json({ message: "not full info" });
+    }
+    db.query('SELECT COUNT(*) as cnt FROM users WHERE username = $1 OR email = $2', [username, email], (err, data) => {
+      if (data && data.rows[0].cnt == 0) {
+        fs.readFile('noimage.png', function (err, image) {
+          if (err) throw err;
+          console.log(password);
+          var passwordToSave = bcrypt.hashSync(password, salt);
+          console.log(passwordToSave);
+          db.query('INSERT INTO users(username, password, email, imagedata) VALUES($1, $2, $3, $4)', [username, passwordToSave, email, image], (err, data) => {
+            db.query('SELECT userid FROM users WHERE username = $1', [username], (err, data) => {
+              if (data.rows[0]) {
+                var payload = { id: data.rows[0].userid };
+                var token = jwt.sign(payload, jwtOptions.secretOrKey);
+                res.json({ token: token });
+              }
+            })
+          })
+        });
+      } else {
+        res.status(400).json({ message: "already registered" });
+      }
+    })
+  });
 };
