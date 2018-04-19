907148a2fbcd9a111cde1659fe0f8aaaa905b948
dima <dima@gmail.com>
2018-04-19 11:57:49 +0300

    fixing vk api

--- vk/api.js
+++ vk/api.js
@@ -56,7 +56,7 @@ var getImages = async (offset) => {
                 const width = body.response.items[0].attachments[0].photo.width;
                 path = body.response.items[0].attachments[0].photo.photo_604;
 
-                response = await request(path, { encoding: null });
+                response = await request(path);
                 console.log(response.body);
                 await db.query('INSERT INTO images(imagedata, source, width, height) VALUES($1, $2, $3, $4)', [response.body, groupName, width, height])
                 
