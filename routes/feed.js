252cfcbfcb47c49afd62622d5696153239432573
dima <dima@gmail.com>
2018-05-17 09:27:58 +0300

    testing

--- routes/feed.js
+++ routes/feed.js
@@ -68,7 +68,7 @@ router.get("/categoriesFeed", passport.authenticate('jwt', {session: false}), as
         const memes = await db.query(`SELECT images.imageid, images.source, images.height, images.width, likes, dislikes, likes.opinion AS opinion FROM images `
         + `LEFT OUTER JOIN likes ON likes.imageid = images.imageid AND likes.userid = $1 `
         + `WHERE EXISTS (SELECT * FROM imagesCategories WHERE images.imageid = imagesCategories.imageid AND (imagesCategories.categoryid = 8 OR imagesCategories.categoryid = 9)) `
-        + `ORDER BY imageid DESC LIMIT $3 OFFSET $4`, [req.user.userid, catstr, count, offset])
+        + `ORDER BY imageid DESC LIMIT $2 OFFSET $3`, [req.user.userid, count, offset])
         
     }
     catch (err){
