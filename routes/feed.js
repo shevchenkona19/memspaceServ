341adaa20c06e8ae305c2eb93e4859716dfeba8c
dima <dima@gmail.com>
2018-05-17 08:45:11 +0300

    testing

--- routes/feed.js
+++ routes/feed.js
@@ -60,10 +60,20 @@ router.get("/categoriesFeed", passport.authenticate('jwt', {session: false}), as
         catstr += 'imagesCategories.categoryid = ' + cat.categoryid + ' OR ';
     });
     catstr = catstr.substring(0, catstr.length - 4);
-    const memes = await db.query(`SELECT images.imageid, images.source, images.height, images.width, likes, dislikes, likes.opinion AS opinion FROM images `
+    try{
+        console.log(`SELECT images.imageid, images.source, images.height, images.width, likes, dislikes, likes.opinion AS opinion FROM images `
         + `LEFT OUTER JOIN likes ON likes.imageid = images.imageid AND likes.userid = $1 `
         + `WHERE EXISTS (SELECT COUNT(*) FROM imagesCategories WHERE images.imageid = imagesCategories.imageid AND ($2) `
         + `ORDER BY imageid DESC LIMIT $3 OFFSET $4`, [req.user.userid, catstr, count, offset])
+        const memes = await db.query(`SELECT images.imageid, images.source, images.height, images.width, likes, dislikes, likes.opinion AS opinion FROM images `
+        + `LEFT OUTER JOIN likes ON likes.imageid = images.imageid AND likes.userid = $1 `
+        + `WHERE EXISTS (SELECT COUNT(*) FROM imagesCategories WHERE images.imageid = imagesCategories.imageid AND ($2) `
+        + `ORDER BY imageid DESC LIMIT $3 OFFSET $4`, [req.user.userid, catstr, count, offset])
+    }
+    catch (err){
+        console.log(err.stack);
+        return res.status(500).json({message: "BD error"});
+    }
     if (memes.rows[0]) {
         return res.status(200).json({memes: memes.rows});
     } else return res.status(200).json({memes: []});
