aec93a75a3797deb19dcc51585352371d9eb1975
dima <dima@gmail.com>
2018-05-17 09:39:21 +0300

    fixing feed

--- routes/feed.js
+++ routes/feed.js
@@ -60,14 +60,12 @@ router.get("/categoriesFeed", passport.authenticate('jwt', {session: false}), as
         catstr += `imagesCategories.categoryid = ` + cat.categoryid + ` OR `;
     });
     catstr = catstr.substring(0, catstr.length - 4);
-    console.log(catstr);
     let memes;
     try{
         memes = await db.query(`SELECT images.imageid, images.source, images.height, images.width, likes, dislikes, likes.opinion AS opinion FROM images `
         + `LEFT OUTER JOIN likes ON likes.imageid = images.imageid AND likes.userid = $1 `
         + `WHERE EXISTS (SELECT * FROM imagesCategories WHERE images.imageid = imagesCategories.imageid AND (${catstr})) `
-        + `ORDER BY imageid DESC LIMIT $2 OFFSET $3`, [req.user.userid, count, offset])
-        
+        + `ORDER BY imageid DESC LIMIT $2 OFFSET $3`, [req.user.userid, count, offset]);   
     }
     catch (err){
         console.log(err.stack);
@@ -77,19 +75,23 @@ router.get("/categoriesFeed", passport.authenticate('jwt', {session: false}), as
         return res.status(200).json({memes: memes.rows});
     } else return res.status(200).json({memes: []});
 });
-//Проверить categoriesFeed и применить для остальных
-//-----------------------------------------------------------------------------------------------------
 router.get("/categoryFeed", passport.authenticate('jwt', {session: false}), async (req, res) => {
-    return res.status(500).json({message: ""});
-    if (!req.query.count || !req.query.offset || !req.query.categoryname) {
+    if (!req.query.count || !req.query.offset || !req.query.categoryid) {
         return res.status(400).json({message: "incorrect query"});
     }
     const count = req.query.count;
     const offset = req.query.offset;
-    const categoryName = req.query.categoryname;
-    const memes = await db.query(`SELECT images.imageid, images.source, images.height, images.width, likes, dislikes, likes.opinion AS opinion `
-        + `FROM images LEFT OUTER JOIN likes ON likes.imageid = images.imageid AND likes.userid = ${req.user.userid} WHERE ${categoryName} = '1' `
+    const categoryid = req.query.categoryid;
+    let memes;
+    try{
+        memes = await db.query(`SELECT images.imageid, images.source, images.height, images.width, likes, dislikes, likes.opinion AS opinion `
+        + `FROM images LEFT OUTER JOIN likes ON likes.imageid = images.imageid AND likes.userid = ${req.user.userid} WHERE `
+        + `EXISTS (SELECT * FROM imagesCategories WHERE images.imageid = imagesCategories.imageid AND categoryid = ${categoryid} `
         + `ORDER BY imageid DESC LIMIT ${count} OFFSET ${offset}`);
+    } catch(err){
+        console.log(err.stack);
+        return res.status(500).json({message: "BD error"});
+    }
     if (memes.rows[0]) {
         return res.json({memes: memes.rows});
     } else return res.json({memes: []});
