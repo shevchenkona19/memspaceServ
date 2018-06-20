646a1fa7879594a598368308d971ac4efe05ce82
Nikita Shevchenko <nikita.shevchenko@firstbridge.io>
2018-06-20 11:36:09 +0300

    added new route

--- routes/favorites.js
+++ routes/favorites.js
@@ -10,19 +10,19 @@ router.post("/addToFavorites", passport.authenticate('jwt', {session: false}), a
     if (!req.query.id) {
         return res.status(401).json({message: "incorrect data"});
     }
-    try{
+    try {
         await db.query(`INSERT INTO favorites(userid, imageid) VALUES($1, $2)`, [req.user.userid, req.query.id]);
     } catch (err) {
         console.log(err.stack);
         return res.status(500).json({message: "BD error"});
-    }   
+    }
     res.status(200).json({message: "200"});
 });
 router.get("/allFavorites", passport.authenticate('jwt', {session: false}), async (req, res) => {
     if (req.user.accesslvl === -1) {
         return res.status(401).json({message: 'unauthorized'})
     }
-    try{
+    try {
         const data = await db.query('SELECT imageid FROM favorites WHERE userid = $1', [req.user.userid]);
         if (data.rows[0]) {
             return res.json({favorites: data.rows});
@@ -41,13 +41,28 @@ router.delete("/removeFromFavorites", passport.authenticate('jwt', {session: fal
     if (!req.query.id) {
         return res.status(401).json({message: "incorrect data"});
     }
-    try{
+    try {
         await db.query(`DELETE FROM favorites WHERE userid = $1 AND imageid = $2`, [req.user.userid, req.query.id]);
     } catch (err) {
         console.log(err.stack);
         return res.status(500).json({message: "BD error"});
-    }  
+    }
     res.status(200).json({message: "200"});
+});
+router.get("/isFavourite", passport.authenticate('jwt', {session: false}), async (req, res) => {
+    if (req.user.accesslvl === -1) {
+        return res.status(401).json({message: 'unauthorized'})
+    }
+    if (!req.query.id) {
+        return res.status(401).json({message: "incorrect data"});
+    }
+    try {
+        const isFavourite = (await db.query("SELECT * FROM favorites WHERE userid = $1 AND imageid = $2", [req.user.userid, req.query.id])).length > 0;
+        return res.status(200).json({isFavourite});
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({message: "DB error"})
+    }
 });
 
 module.exports = router;
