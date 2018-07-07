5c9275c9fd972b077d3a81bafee2a43e386ba0dc
Nikita Shevchenko <nikita.shevchenko@firstbridge.io>
2018-07-07 19:39:19 +0300

    update clearing alg not to delete favorites

--- src/controllers/cleaner.js
+++ src/controllers/cleaner.js
@@ -1,4 +1,5 @@
 const Images = require("../model/index").getImagesModel();
+const Favorites = require("../model/index").getFavoritesModel();
 const Sequelize = require("sequelize").Op;
 const moment = require("moment");
 const fs = require("fs");
@@ -6,11 +7,16 @@ const fs = require("fs");
 async function clearOldMemes() {
     const memesToDelete = await Images.findAll({where: {createdAt: {[Sequelize.lt]: moment().subtract(30, "days").toDate()}}});
     const deleted = memesToDelete.length;
+    const confirm = memesToDelete.filter(async mem => {
+        const isFavorite = await Favorites.findOne({where: {imageId: mem.imageid}});
+        return isFavorite !== null;
+    });
+    const ids = confirm.map(mem => mem.imageId);
     try {
-        memesToDelete.forEach(mem => {
+        confirm.forEach(async mem => {
             fs.unlinkSync(mem.imageData);
         });
-        await Images.destroy({where: {createdAt: {[Sequelize.lt]: moment().subtract(30, "days").toDate()}}});
+        await Images.destroy({where: {imageId: ids}});
     } catch (e) {
         console.error("Error in clearing memes!", e.stack);
     }
